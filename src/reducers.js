599be6a8813b3f0cbfc0c266699802eda59c89a6
DESKTOP-SG1ICAK\vbsav <vb7634@gmail.com>
2020-07-15 19:29:02 -0400

    Revert "Create user data with graphql page"
    This reverts commit b946c3d11a6b1a7b217beaf82b38b9d5763c942d.

--- src/reducers.js
+++
@@ -1,9 +0,0 @@
-const initState = [];
-
-export default function reducer(state = initState, action) {
-    switch(action.type) {
-        case "GET_DATA":
-            return state = action.data;
-    }
-    return state;
-}
